b71c4877140b116e1a21ea01c16feab86f15075e
Ringo0731 <ringoee3320@gmail.com>
2019-09-25 12:38:21 +0900

    fixed small problems

--- MergeSort.c
+++ MergeSort.c
@@ -2,34 +2,34 @@
 
 #define MAX_ARRAY 100
 
-int is1stSmall(void* a,void* b){
+int is_1st_small(void* a,void* b){
     int A = *(int*)a;
     int B = *(int*)b;
-    return (A < B) ? 1 : -1;
+    return (A < B);
 }
 
-void MergeSort(void* array[],int Left,int Right,int (*mustSwap)(void*,void*)){
+void MergeSort(void* array[],int left,int right,int (*must_swap)(void*,void*)){
     
     int i,j,k,center;
     void* work[MAX_ARRAY];
 
-    if(Left<Right){
-        center = (Left + Right)/2;
-        MergeSort(array,Left,center,mustSwap);
-        MergeSort(array,center+1,Right,mustSwap);
+    if(left<right){
+        center = (left + right)/2;
+        MergeSort(array,left,center,must_swap);
+        MergeSort(array,center+1,right,must_swap);
 
-        for(i = center;i >= Left;i--){
+        for(i = center;i >= left;i--){
             work[i] = array[i];
         }
-        for(j = center + 1;j <= Right;j++){
-            work[Right-(j-(center+1))] = array[j];
+        for(j = center + 1;j <= right;j++){
+            work[right-(j-(center+1))] = array[j];
         }
 
-        i = Left;
-        j = Right;
+        i = left;
+        j = right;
 
-        for(k = Left;k <= Right;k++){
-            if((*mustSwap)(work[i],work[j]) > 0){
+        for(k = left;k <= right;k++){
+            if((*must_swap)(work[i],work[j]) > 0){
                 array[k] = work[i++];
             }else{
                 array[k] = work[j--];
@@ -41,9 +41,9 @@ void MergeSort(void* array[],int Left,int Right,int (*mustSwap)(void*,void*)){
 int main(void){
 
     int array[MAX_ARRAY];
-    void* Sorted_array[MAX_ARRAY];
+    void* sorted_array[MAX_ARRAY];
     int N,i;
-    int (*mustSwap)(void*,void*) = is1stSmall;
+    int (*must_swap)(void*,void*) = is_1st_small;
 
     scanf("%d",&N);
     for(i = 0;i < N;i++){
@@ -51,14 +51,14 @@ int main(void){
     }
 
     for(i = 0;i < N;i++){
-        Sorted_array[i] = (void*)&array[i];
+        sorted_array[i] = (void*)&array[i];
     }
 
-    MergeSort(Sorted_array,0,N-1,mustSwap);
+    MergeSort(sorted_array,0,N-1,must_swap);
 
     printf("ソート結果：");
     for(i = 0;i < N;i++){
-        printf("%d ",*(int*)Sorted_array[i]);
+        printf("%d ",*(int*)sorted_array[i]);
     }
 
     printf("\n");
